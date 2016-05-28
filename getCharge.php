f0816f1ff47201eb554db948e0b2a9b2db7045ec
maxwell_mandela <maxywhyner94@gmail.com>
2016-05-28 13:16:01 +0300

    getCharge Works

--- getCharge.php
+++ getCharge.php
@@ -7,4 +7,18 @@ $xmlData = simplexml_load_string($xml);
 
 $newsXML = new SimpleXMLElement($xml);
 Header('Content-type: text/xml');
-echo $newsXML->asXML();
+$xml_data = $newsXML->asXML();
+
+$URL = "http://demo.mtmlive.net/webservice/api/xml/reply/GetMTCharge";
+ 
+$ch = curl_init($URL);
+curl_setopt($ch, CURLOPT_MUTE, 1);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: text/xml'));
+curl_setopt($ch, CURLOPT_POSTFIELDS, "$xml_data");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$output = curl_exec($ch);
+echo $output;
+curl_close($ch);
