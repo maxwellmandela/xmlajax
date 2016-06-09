85a0d9347c4882a3a68dc94d5b29c4fc4c9b1e94
maxwell_mandela <maxywhyner94@gmail.com>
2016-06-09 23:57:07 +0300

    finally

--- addCustomer.php
+++ addCustomer.php
@@ -1,19 +1,14 @@
 <?php
-/*$xml = file_get_contents('php://input');
-$xmlData = simplexml_load_string($xml);
+$xml = file_get_contents('php://input');
 //echo $xml;
 
-$newXml = new SimpleXMLElement($xml);
-Header('Content-type: text/xml');
-$xml_data = $newXml->asXML();
-*/
-$newsXML = new SimpleXMLElement("<AddCustomer xmlns:i='http://www.w3.org/2001/XMLSchema-instance' xmlns='http://schemas.datacontract.org/2004/07/MTM.DTO'></AddCustomer>");
-$newsXML->addChild('ClientPassword', 'some value');
-$newsXML->addChild('ClientUsername', 'Max');
-$newsXML->addChild('BaseCurrency', 'some values');
-$newsXML->addChild('Country', 'Maxs');
-Header('Content-type: text/xml');
-$xml_data = $newsXML->asXML();
+try {
+    $new_data = new SimpleXMLElement($xml);
+    Header('Content-type: text/xml');
+    $new_xml_data = $new_data->asXML();
+}catch (Exception $e){
+    print $e->getMessage();
+}
 
 $URL = "http://demo.mtmlive.net/webservice/api/xml/reply/AddCustomer";
 
@@ -23,7 +18,8 @@ curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: text/xml'));
-curl_setopt($ch, CURLOPT_POSTFIELDS, "$xml_data");
+curl_setopt($ch, CURLOPT_USERPWD, "administrator:password");
+curl_setopt($ch, CURLOPT_POSTFIELDS, "$new_xml_data");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $output = curl_exec($ch);
 echo $output;
--- validateLogin.php
+++ validateLogin.php
@@ -2,11 +2,13 @@
 $xml = file_get_contents('php://input');
 $xmlData = simplexml_load_string($xml);
 //echo $xml;
-
-$newsXML = new SimpleXMLElement($xml);
-Header('Content-type: text/xml');
-$xml_data = $newsXML->asXML();
-
+try {
+    $newsXML = new SimpleXMLElement($xml);
+    Header('Content-type: text/xml');
+    $xml_data = $newsXML->asXML();
+}catch (Exception $e){
+    print $e->getMessage();
+}
 $URL = "http://demo.mtmlive.net/webservice/api/xml/reply/ValidateLogin";
 
 $ch = curl_init($URL);
@@ -15,6 +17,7 @@ curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: text/xml'));
+curl_setopt($ch, CURLOPT_USERPWD, "administrator:password");
 curl_setopt($ch, CURLOPT_POSTFIELDS, "$xml_data");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $output = curl_exec($ch);
